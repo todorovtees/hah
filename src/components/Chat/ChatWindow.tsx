ef8abaced0a7ac7c88ad5a11c4296df4a4506846
todorovtees <office@todorovtees.com>
2026-08-30 13:11:00 +0300

    Fix ChatWindow layout and add missing newline

--- src/components/Chat/ChatWindow.tsx
+++ src/components/Chat/ChatWindow.tsx
@@ -120,7 +120,7 @@ export function ChatWindow({ conversationId, ensureConversationId, onExchangeCom
   }
 
   return (
-    <div className="flex min-w-0 flex-1 flex-col">
+    <div className="flex min-h-0 min-w-0 flex-1 flex-col">
       <MessageList messages={messages} errorText={streamError} />
       <Composer
         value={draft}
